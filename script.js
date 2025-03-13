cb3e12de7ea217abade86d09b2e450f88622e484
Joris Lefait <jorislefait@MacBook-Pro-de-Joris.local>
2025-03-13 09:28:24 +0100

    navbar responsive

--- script.js
+++ script.js
@@ -259,3 +259,19 @@ if (skills.length > 0) {
     `;
   });
 }
+
+// Handle navbar mobile
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-menu a").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
